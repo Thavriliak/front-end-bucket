3b189a01f108ddd1566b3d176de08e0524c80939
Thavriliak <Havriliakt@gmail.com>
2018-12-17 15:58:37 -0500

    Got a map to show up on our application
    Map is enabled using leaflet in conjunction with OpenStreetMap

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ require('./assets/scripts/app.js')
 
 // styles
 require('./assets/styles/index.scss')
+
+const mymap = L.map('mapid').setView([51.505, -0.09], 13)
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2hlZWJpZXNkZWViaWVzIiwiYSI6ImNqcHNzcXJucTAwYzQ0OWw3YjYwcXZ5YTAifQ.HvPxZ9R5zMQqqSw-8bnu9A', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'your.mapbox.access.token'
+}).addTo(mymap)
+const marker = L.marker([51.5, -0.09]).addTo(mymap)
+let circle = L.circle([51.508, -0.11], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(mymap)
+const polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(mymap)
